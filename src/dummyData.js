5f9b037dc29db4f6feffd5e3752fe19e6800c660
Vladut Dascaleanu <vladutdascaleanultd@gmail.com>
2023-10-02 18:54:02 +0100

    another commit

--- src/dummyData.js
+++ src/dummyData.js
@@ -9,8 +9,8 @@ export const homeData = [
         stars: "James Cameron, Rick Jaffa, Amanda Silver",
         genres: "Action",
         tags: "Action, Sci-Fi, Fantasy",
-        cover: "images/movie-adventure.jpeg",
-        videoId: "videos/video1.mp4",
+        cover: "/images/movie-adventure.jpeg",
+        videoId: "/videos/video1.mp4",
         date: "22-December-2022",
     },
     {
@@ -22,7 +22,7 @@ export const homeData = [
         stars: "Josephine Langford, Hero Fiennes Tiffin, Selma Blair",
         genres: "Drama",
         tags: "Drama, Romance",
-        cover: "images/movie-romance1.jpeg",
+        cover: "/images/movie-romance1.jpeg",
         videoId: "videos/video2.mp4",
         date: "12-May-2023",
     },
@@ -36,8 +36,8 @@ export const homeData = [
         stars: "Jennifer Lawrence, Andrew Barth, Natalie Morales",
         genres: "Action",
         tags: "Comedy, Romance",
-        cover: "images/movie-comedy1.jpeg",
-        videoId: "videos/video3.mp4",
+        cover: "/images/movie-comedy1.jpeg",
+        videoId: "/videos/video3.mp4",
         date: "23-June-2023",
     },
 
@@ -50,8 +50,8 @@ export const homeData = [
         stars: "Halle Bailey, Javier Bardem, Jonah Hauer-King",
         genres: "Fantasy",
         tags: "Adventures, Family, Fantasy, Musical",
-        cover: "images/movie-family1.jpeg",
-        videoId: "videos/video4.mp4",
+        cover: "/images/movie-family1.jpeg",
+        videoId: "/videos/video4.mp4",
         date: "26-May-2023",
     },
 
@@ -64,8 +64,8 @@ export const homeData = [
         stars: "Ben Hardy, Haley Lu Richardson, Jameela Jamil",
         genres: "Drama",
         tags: "Drama, Romance",
-        cover: "../images/movie-love1.jpeg",
-        videoId: "../videos/video5.mp4",
+        cover: "/images/movie-love1.jpeg",
+        videoId: "/videos/video5.mp4",
         date: "15-September-2023",
     },
 
@@ -78,8 +78,8 @@ export const homeData = [
         stars: "Dwayne Johnson, Neve Campbell, Pablo Schreiber",
         genres: "Action",
         tags: "Action, Thriller",
-        cover: "../images/movie-thriller1.jpeg",
-        videoId: "../videos/video6.mp4",
+        cover: "/images/movie-thriller1.jpeg",
+        videoId: "/videos/video6.mp4",
         date: "12-May-2023",
     },
 
@@ -92,8 +92,8 @@ export const homeData = [
         stars: "Ben Affleck, Alice Braga, Jeff Fahey",
         genres: "Action",
         tags: "Action, Adventures, Family",
-        cover: "../images/movie-action1.webp",
-        videoId: "../videos/video7.mp4",
+        cover: "/images/movie-action1.webp",
+        videoId: "/videos/video7.mp4",
         date: "12-May-2023",
     },
 
@@ -106,8 +106,8 @@ export const homeData = [
         stars: "Jimmy Wong, John, Jackie Chan",
         genres: "Animation",
         tags: "Animation, Adventure, Comedy",
-        cover: "../images/movie-animation1.jpeg",
-        videoId: "../videos/video8.mp4",
+        cover: "/images/movie-animation1.jpeg",
+        videoId: "/videos/video8.mp4",
         date: "15-January-2021",
     },
 
@@ -121,8 +121,8 @@ export const homeData = [
         stars: "Tom Hardy, Michelle Williams, Riz Ahmed",
         genres: "Action",
         tags: "Action, Sci-Fi, Adventure",
-        cover: "../images/movie-action2.jpeg",
-        videoId: "../videos/video9.mp4",
+        cover: "/images/movie-action2.jpeg",
+        videoId: "/videos/video9.mp4",
         date: "03-October-2018",
     },
 
@@ -136,8 +136,8 @@ export const homeData = [
         stars: "Tom Cruise, Jennifer Connelly, Miles Teller",
         genres: "Action",
         tags: "Action, Adventure, Drama",
-        cover: "../images/movie-family2.jpeg",
-        videoId: "../videos/video10.mp4",
+        cover: "/images/movie-family2.jpeg",
+        videoId: "/videos/video10.mp4",
         date: "27-May-2022",
     },
 
@@ -151,8 +151,8 @@ export const homeData = [
         stars: "Harrison Ford, Phoebe Waller-Bridge, Antonio Banderas",
         genres: "Adventure",
         tags: "Action, Adventure, Sci-Fi",
-        cover: "../images/movie-adventure2.jpeg",
-        videoId: "../videos/video11.mp4",
+        cover: "/images/movie-adventure2.jpeg",
+        videoId: "/videos/video11.mp4",
         date: "28-June-2023",
     },
 
@@ -166,8 +166,8 @@ export const homeData = [
         stars: "Jennifer Lopez, Lucy Paex, Omari Hardwick",
         genres: "Action",
         tags: "Action, Adventure, Thriller",
-        cover: "../images/movie-action3.jpeg",
-        videoId: "../videos/video12.mp4",
+        cover: "/images/movie-action3.jpeg",
+        videoId: "/videos/video12.mp4",
         date: "12-May-2023",
     },
 
@@ -178,70 +178,70 @@ export const popularData = [
 
     {
         id: 1,
-        cover: "../images/popular-movies/movie1.jpeg",
+        cover: "/images/popular-movies/movie1.jpeg",
     name: "Avatar",
     time: "3hr : 10mins",
     },
 
     {
         id: 2,
-        cover: "../images/popular-movies/movie2.jpeg",
+        cover: "/images/popular-movies/movie2.jpeg",
         name: "The Equalizer III",
         time: "2hr : 12mins",
     },
 
     {
         id: 3,
-        cover: "images/popular-movies/movie3.jpeg",
+        cover: "/images/popular-movies/movie3.jpeg",
         name: "About Time",
         time: "2hr : 04mins",
     },
 
     {
         id: 4,
-        cover: "images/popular-movies/movie4.jpeg",
+        cover: "/images/popular-movies/movie4.jpeg",
         name: "Every Day",
         time: "1hr : 37mins",
     },
 
     {
         id: 5,
-        cover: "images/popular-movies/movie5.jpeg",
+        cover: "/images/popular-movies/movie5.jpeg",
         name: "Holidate",
         time: "1hr : 47mins",
     },
 
     {
         id: 6,
-        cover: "../images/popular-movies/movie6.jpeg",
+        cover: "/images/popular-movies/movie6.jpeg",
         name: "6 Underground",
         time: "2hr : 8mins",
     },
 
     {
         id: 7,
-        cover: "../images/popular-movies/movie7.jpeg",
+        cover: "/images/popular-movies/movie7.jpeg",
         name: "John Wick 2",
         time: "2hr : 2mins",
     },
 
     {
         id: 8,
-        cover: "../images/popular-movies/movie8.jpeg",
+        cover: "/images/popular-movies/movie8.jpeg",
         name: "In Time",
         time: "1hr : 43mins",
     },
 
     {
         id: 9,
-        cover: "../images/popular-movies/movie9.jpeg",
+        cover: "/images/popular-movies/movie9.jpeg",
         name: "Onward",
         time: "1hr : 49mins",
     },
 
     {
         id: 10,
-        cover: "../images/popular-movies/movie10.jpeg",
+        cover: "/images/popular-movies/movie10.jpeg",
         name: "Tangled",
         time: "1hr : 33mins",
     },
@@ -253,70 +253,70 @@ export const latestData = [
 
     {
         id: 1,
-        cover: "../images/latest-movie/movie1.jpeg",
+        cover: "/images/latest-movie/movie1.jpeg",
         name: "Heart of stone",
         time: "2hr : 02mins",
     },
 
     {
         id: 2,
-        cover: "../images/latest-movie/movie2.jpeg",
+        cover: "/images/latest-movie/movie2.jpeg",
         name: "The Covenant",
         time: "2hr : 03mins",
     },
 
     {
         id: 3,
-        cover: "../images/latest-movie/movie3.jpeg",
+        cover: "/images/latest-movie/movie3.jpeg",
         name: "The black demon",
         time: "1hr : 40mins",
     },
 
     {
         id: 4,
-        cover: "../images/latest-movie/movie4.jpeg",
+        cover: "/images/latest-movie/movie4.jpeg",
         name: "The Grinch",
         time: "1hr : 30mins",
     },
 
     {
         id: 5,
-        cover: "../images/latest-movie/movie5.jpeg",
+        cover: "/images/latest-movie/movie5.jpeg",
         name: "A good person",
         time: "2hr : 25mins",
     },
 
     {
         id: 6,
-        cover: "../images/latest-movie/movie6.jpeg",
+        cover: "/images/latest-movie/movie6.jpeg",
         name: "Barbie",
         time: "1hr : 54mins",
     },
 
     {
         id: 7,
-        cover: "../images/latest-movie/movie7.jpeg",
+        cover: "/images/latest-movie/movie7.jpeg",
         name: "Vivo",
         time: "1hr : 43mins",
     },
 
     {
         id: 8,
-        cover: "../images/latest-movie/movie8.jpeg",
+        cover: "/images/latest-movie/movie8.jpeg",
         name: "Purple Heart",
         time: "2hr : 02mins",
     },
 
     {
         id: 9,
-        cover: "../images/latest-movie/movie9.jpeg",
+        cover: "/images/latest-movie/movie9.jpeg",
         name: "Pride and Prejudice",
         time: "2hr : 07mins",
     },
 
     {
         id: 10,
-        cover: "../images/latest-movie/movie10.jpeg",
+        cover: "/images/latest-movie/movie10.jpeg",
         name: "Kandahar",
         time: "2hr : 00mins",
     },
@@ -327,7 +327,7 @@ export const watchtData = [
 
     {
         id: 1,
-        cover: "../images/watch-movie/movie1.jpeg",
+        cover: "/images/watch-movie/movie1.jpeg",
         name: "Spiderhead",
         time: "1hr : 46mins",
     },
@@ -335,7 +335,7 @@ export const watchtData = [
   
     {
         id: 2,
-        cover: "../images/watch-movie/movie2.jpeg",
+        cover: "/images/watch-movie/movie2.jpeg",
         name: "American Sniper",
         time: "2hr : 13mins",
     },
@@ -344,21 +344,21 @@ export const watchtData = [
  
     {
         id: 3,
-        cover: "../images/watch-movie/movie3.jpeg",
+        cover: "/images/watch-movie/movie3.jpeg",
         name: "The Dawn Wall",
         time: "1hr : 40mins",
     },
 
     {
         id: 4,
-        cover: "../images/watch-movie/movie4.jpeg",
+        cover: "/images/watch-movie/movie4.jpeg",
         name: "Transformers: Rise of the Beasts",
         time: "2hr : 07mins",
     },
 
     {
         id: 5,
-        cover: "../images/watch-movie/movie5.jpeg",
+        cover: "/images/watch-movie/movie5.jpeg",
         name: "Last Man Down",
         time: "1hr : 27mins",
     },
@@ -367,7 +367,7 @@ export const watchtData = [
    
     {
         id: 6,
-        cover: "../images/watch-movie/movie6.jpeg",
+        cover: "/images/watch-movie/movie6.jpeg",
         name: "The Boss Baby",
         time: "1hr : 37mins",
     },
@@ -376,7 +376,7 @@ export const watchtData = [
   
     {
         id: 7,
-        cover: "../images/watch-movie/movie7.jpeg",
+        cover: "/images/watch-movie/movie7.jpeg",
         name: "The Man from Toronto",
         time: "1hr : 50mins",
     },
@@ -385,7 +385,7 @@ export const watchtData = [
   
     {
         id: 8,
-        cover: "../images/watch-movie/movie8.jpeg",
+        cover: "/images/watch-movie/movie8.jpeg",
         name: "Clifford the Big Red Dog",
         time: "1hr : 36mins",
     },
@@ -393,7 +393,7 @@ export const watchtData = [
    
     {
         id: 9,
-        cover: "../images/watch-movie/movie9.jpeg",
+        cover: "/images/watch-movie/movie9.jpeg",
         name: "Elemental",
         time: "1hr : 41mins",
     },
@@ -401,7 +401,7 @@ export const watchtData = [
 
     {
         id: 10,
-        cover: "../images/watch-movie/movie10.jpeg",
+        cover: "/images/watch-movie/movie10.jpeg",
         name: "Killers Of The Flower Moon",
         time: "3hr : 26mins",
     },
@@ -506,70 +506,70 @@ export const recommendedData = [
 
     {
         id: 1,
-        cover: "../images/recommended-movie/movie1.jpeg",
+        cover: "/images/recommended-movie/movie1.jpeg",
         name: "Top Gun: Maverick",
         time: "2hr : 11mins",
     },
 
     {
         id: 2,
-        cover: "../images/recommended-movie/movie2.jpeg",
+        cover: "/images/recommended-movie/movie2.jpeg",
         name: "Wish Dragon",
         time: "1hr : 38mins",
     },
 
     {
         id: 3,
-        cover: "../images/recommended-movie/movie3.jpeg",
+        cover: "/images/recommended-movie/movie3.jpeg",
         name: "The gentleman driver",
         time: "1hr : 23mins",
     },
 
     {
         id: 4,
-        cover: "../images/recommended-movie/movie4.jpeg",
+        cover: "/images/recommended-movie/movie4.jpeg",
         name: "See you soon",
         time: "1hr : 47mins",
     },
 
     {
         id: 5,
-        cover: "../images/recommended-movie/movie5.jpeg",
+        cover: "/images/recommended-movie/movie5.jpeg",
         name: "A million miles away",
         time: "2hr : 01mins",
     },
 
     {
         id: 6,
-        cover: "../images/recommended-movie/movie6.jpeg",
+        cover: "/images/recommended-movie/movie6.jpeg",
         name: "Greyhound",
         time: "1hr : 31mins",
     },
 
     {
         id: 7,
-        cover: "../images/recommended-movie/movie7.jpeg",
+        cover: "/images/recommended-movie/movie7.jpeg",
         name: "Happiness for beginners",
         time: "1hr : 43mins",
     },
 
     {
         id: 8,
-        cover: "../images/recommended-movie/movie8.jpeg",
+        cover: "/images/recommended-movie/movie8.jpeg",
         name: "A star is born",
         time: "2hr : 15mins",
     },
 
     {
         id: 9,
-        cover: "../images/recommended-movie/movie9.jpeg",
+        cover: "/images/recommended-movie/movie9.jpeg",
         name: "The Northman",
         time: "2hr : 17mins",
     },
 
     {
         id: 10,
-        cover: "../images/recommended-movie/movie10.jpeg",
+        cover: "/images/recommended-movie/movie10.jpeg",
         name: "Only God Forgives",
         time: "1hr : 30mins",
     },
